e2a76850c3e54306d059e7edee25e9b1853d27d8
ardhnarishwar-ai <solution.funda@gmail.com>
2026-07-05 03:58:38 +0530

    Add VTGD Constants

--- frontend/src/engine/core/constants.ts
+++ frontend/src/engine/core/constants.ts
@@ -1,1 +1,19 @@
+export const VTGD = {
 
+    VERSION: "2.0",
+
+    NEUTRAL_WEIGHT: 0,
+
+    MAX_WEIGHT: 1,
+
+    MIN_WEIGHT: -1,
+
+    PCR_BULLISH: 1.20,
+
+    PCR_BEARISH: 0.80,
+
+    SAFETY_LIMIT: 0.75,
+
+    DIFFUSION_LIMIT: 0.60
+
+};
