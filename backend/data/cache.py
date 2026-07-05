a761e1ce87b40d5bd987c140b9bbeda1c6d07235
ardhnarishwar-ai <solution.funda@gmail.com>
2026-07-05 21:03:13 +0530

    Implement TickCache class for storing tick data

--- backend/data/cache.py
+++ backend/data/cache.py
@@ -1,1 +1,18 @@
+"""
+VTGD Tick Cache
+"""
 
+
+class TickCache:
+
+    def __init__(self):
+
+        self.previous = {}
+
+    def update(self, symbol, data):
+
+        self.previous[symbol] = data
+
+    def get(self, symbol):
+
+        return self.previous.get(symbol)
